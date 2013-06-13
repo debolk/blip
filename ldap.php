b45b88c7dfc9abc3f8ef1fc0e8431eff6c4d9f7d
Jakob Buis <jpabuis@gmail.com>
2013-06-13 23:19:20 +0200

    improved method description of the LDAP-class

--- ldap.php
+++ ldap.php
@@ -24,21 +24,42 @@ class LDAP
     $this->server = new Zend\Ldap\Ldap($options);
   }
 
+  /**
+   * Find the information of every member
+   * @return array[object]
+   */
   public function find_all()
   {
     throw new Exception('Method not implemented');
   }
 
+  /**
+   * Find the information of a specific member
+   * @param int $id the id of the member to find
+   * @throws LDAPNotFoundException if the member doesn't exist
+   * @return object
+   */
   public function find($id)
   {
     throw new Exception('Method not implemented');
   }
 
+  /**
+   * Create a new member with the given data
+   * @param array $data
+   * @return the results of LDAP::find() of the new member
+   */
   public function create($data)
   {
     throw new Exception('Method not implemented');
   }
 
+  /**
+   * Update an existing member with the given data
+   * @param array $data
+   * @return the results of LDAP::find() of the member
+   * @throws LDAPNotFoundException if the member doesn't exist
+   */
   public function update($data)
   {
     throw new Exception('Method not implemented');

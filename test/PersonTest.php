dc9912ba4a3a2931b2a2e49683f15c95f795f317
Jakob Buis <jpabuis@gmail.com>
2013-06-16 18:01:28 +0200

    marked tests as incomplete

--- test/PersonTest.php
+++ test/PersonTest.php
@@ -4,21 +4,21 @@ class PersonTest extends PHPUnit_Framework_TestCase
 {
   function test_it_returns_all_users()
   {
-    $this->fail('not implemented');
+    $this->markTestIncomplete('This test has not been implemented yet.');
   }
 
   function test_it_returns_a_specific_user()
   {
-    $this->fail('not implemented');
+    $this->markTestIncomplete('This test has not been implemented yet.');
   }
 
   function test_it_creates_a_new_user()
   {
-    $this->fail('not implemented');
+    $this->markTestIncomplete('This test has not been implemented yet.');
   }
 
   function test_it_updates_an_existing_user()
   {
-    $this->fail('not implemented');
+    $this->markTestIncomplete('This test has not been implemented yet.');
   }
 }

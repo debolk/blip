e5159a63a8a803e25427a830c968af5ef9d3cc6a
Jakob Buis <jpabuis@gmail.com>
2013-06-16 16:02:10 +0200

    outlined tests

--- test/LDAPTest.php
+++ test/LDAPTest.php
@@ -2,5 +2,43 @@
 
 class LDAPTest extends PHPUnit_Framework_TestCase
 {
-  
+  function test_it_setups_a_connection_to_the_server()
+  {
+    $this->fail('not implemented');
+  }
+
+  function test_it_finds_all_users()
+  {
+    $this->fail('not implemented');
+  }
+
+  function test_it_finds_a_specific_user()
+  {
+    $this->fail('not implemented');
+  }
+
+  function test_it_does_not_find_a_nonexisting_user()
+  {
+    $this->fail('not implemented');
+  }
+
+  function test_it_creates_a_new_valid_user()
+  {
+    $this->fail('not implemented');
+  }
+
+  function test_it_does_not_create_an_invalid_user()
+  {
+    $this->fail('not implemented');
+  }
+
+  function test_it_updates_a_user()
+  {
+    $this->fail('not implemented');
+  }
+
+  function test_it_does_not_store_an_invalid_update()
+  {
+    $this->fail('not implemented');
+  }
 }
--- test/PersonTest.php
+++ test/PersonTest.php
@@ -2,5 +2,23 @@
 
 class PersonTest extends PHPUnit_Framework_TestCase
 {
-  
+  function test_it_returns_all_users()
+  {
+    $this->fail('not implemented');
+  }
+
+  function test_it_returns_a_specific_user()
+  {
+    $this->fail('not implemented');
+  }
+
+  function test_it_creates_a_new_user()
+  {
+    $this->fail('not implemented');
+  }
+
+  function test_it_updates_an_existing_user()
+  {
+    $this->fail('not implemented');
+  }
 }

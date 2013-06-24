78c73f0bcd8555cf04913d71dded45dc061da2d7
Jakob Buis <jpabuis@gmail.com>
2013-06-24 18:32:06 +0200

    implemented members/current, not sure of other breakages

--- LDAP.php
+++ LDAP.php
@@ -24,7 +24,7 @@ class LDAP
    */
   public function find_all()
   {
-    $result = $this->ldap_find('(&(objectClass=iNetOrgPerson)(!(objectClass=gosaUserTemplate)))', array('uid', 'givenname', 'sn', 'mail'));
+    $result = $this->server_find('(&(objectClass=iNetOrgPerson)(!(objectClass=gosaUserTemplate)))', array('uid', 'givenname', 'sn', 'mail'));
     return array_map(array($this, 'to_resource'), $result);
   }
 
@@ -44,8 +44,7 @@ class LDAP
    */
   public function find_current_members()
   {
-    // Rest of the method not yet implemented
-    throw new Exception('Method not implemented');
+    return $this->ldap_group_members('cn=leden,ou=groups,o=nieuwedelft');
   }
 
   /**
@@ -143,21 +142,69 @@ class LDAP
    * Performs a LDAP search
    * @param string $query the query to search
    * @param array[string] $attributes attributes to include in the result set
+   * @param string $dn_prefix an extra part to prefix for the base_dn as defined in the configuration
    * @return LDAP result set
    */
-  private function ldap_find($query, $attributes = array())
+  private function ldap_find($query, $attributes = null, $dn_prefix = '')
   {
+    // Prefix the dn to search to enable extension
+    if ($dn_prefix !== '') {
+      $dn_prefix = ','.$dn_prefix;
+    }
+    $dn = $dn_prefix.getenv('LDAP_BASEDN');
+
     // Retrieve results
-    if (sizeof($attributes) > 0) {
-      $search = ldap_search($this->server, getenv('LDAP_BASEDN'), $query, array('uid', 'givenname', 'sn', 'mail'));
-    }
-    else {
-      $search = ldap_search($this->server, getenv('LDAP_BASEDN'), $query);
-    }
+    $search = ldap_search($this->server, $dn, $query, $attributes);
     $result = ldap_get_entries($this->server, $search);
 
     // Remove the first, useless entry
     array_shift($result);
     return $result;
+  }
+
+  /**
+   * Returns all members of a specific group
+   * @param $dn the DN of the group to find (excluding BASE_DN)
+   * @return array[Person]
+   */
+  private function ldap_group_members($dn)
+  {
+    // Find the group
+    $search = ldap_search($this->server, $dn.','.getenv('LDAP_BASEDN'), '(objectClass=PosixGroup)', array('memberuid'));
+    $entries = ldap_get_entries($this->server, $search);
+
+    // Locate the member uids
+    $results = $entries[0]['memberuid'];
+
+    // Ignore the first, useless count entry
+    array_shift($results);
+
+    // Find actual user objects
+    $members = array();
+    foreach ($results as $uid) {
+      $member = $this->find_member($uid);
+      if ($member !== null) {
+        array_push($members, $member);
+      }
+    }
+
+    return $members;
+  }
+
+  /**
+   * Find a member in LDAP and returns its corresponding model
+   * @param string $uid the UID of the user to find
+   * @return Models\Person
+   */
+  private function find_member($uid)
+  {
+    $search = $this->ldap_find("(&(uid=$uid)(objectClass=iNetOrgPerson)(!(objectClass=gosaUserTemplate)))", array('uid', 'givenname', 'sn', 'mail'));
+    // No results
+    if ($search == array()) {
+      return null;
+    }
+    else {
+      return $this->to_resource($search[0]);
+    }
   }
 }

078ec113d684479fbcbed34ec70f952edb9445d8
Jakob Buis <jpabuis@gmail.com>
2015-05-09 19:47:19 +0200

    new endpoint /members/list with limited data

--- Member.php
+++ Member.php
@@ -23,7 +23,33 @@ class MemberCollection extends BlipResource
       case 'current': { return $this->current(); break; }
       case 'candidate': { return $this->candidate(); break; }
       case 'past': { return $this->past(); break; }
+      case 'list': { return $this->list(); break; }
     }
+  }
+
+  /**
+   * Return a list of all members
+   */
+  private function list()
+  {
+    return Helper\Memcache::cache('members_list', function(){
+      $groups = array(
+        'cn=leden,ou=groups,o=nieuwedelft,dc=bolkhuis,dc=nl',
+        'cn=kandidaatleden,ou=groups,o=nieuwedelft,dc=bolkhuis,dc=nl',
+      );
+
+      $people = Models\LdapGroup::peopleInGroups($groups);
+
+      // Filter to the desired attributes
+      $people = array_map(function($person){
+        $t = new stdClass();
+        $t->uid = $person->uid;
+        $t->name = $person->name;
+        return $t;
+      }, $people);
+
+      return new Tonic\Response(200, json_encode($people, JSON_UNESCAPED_SLASHES));
+    });
   }
 
   private function all()

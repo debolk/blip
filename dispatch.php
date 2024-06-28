0350eb0c7900f5e912930c673a092ea584cbfcc8
Alchemi1963 <alchemi1999@hotmail.com>
2024-06-28 00:42:52 +0000

    Setup controllers in dispatch.php

--- dispatch.php
+++ dispatch.php
@@ -17,17 +17,17 @@ $app = Slim\Factory\AppFactory::create();
 //ictcom: members of beheer, bestuur or ictcom
 //lid: members of the society (does not include candidate-members)
 //bekend: members and candidate-members of the society
-$app->get('/persons', array($cont, 'processRequest')); //return all persons with basic info
-$app->get('/persons/all', array($cont, 'processRequest')); //return all persons with all information, ex avg
-$app->post('/person', array($cont, 'processRequest')); //create new person
-$app->get('/person/{uid}', array($cont, 'processRequest')); //return person with basic info
-$app->get('/person/{uid}/all', array($cont, 'processRequest')); //return person with alll info ex avg
-$app->get('/person/{uid}/photo/{width}/{height}', array($cont, 'processRequest')); //return persons profile picture
-$app->patch('/person/{uid}/update', array($cont, 'processRequest')); //update person information
-$app->get('/members', array($cont, 'processRequest')); //return all members with basic info
-$app->get('/members/all', array($cont, 'processRequest')); //return all members with all info ex avg
-$app->get('/members/current', array($cont, 'processRequest')); //return all current members with basic info
-$app->get('/members/former', array($cont, 'processRequest')); //return all former members with basic info
-$app->get('/members/candidate', array($cont, 'processRequest')); //return all candidate members with basic info
+$app->get('/persons', 'controllers\PersonController::route'); //return all persons with basic info
+$app->get('/persons/all', 'controllers\PersonController::route'); //return all persons with all information, ex avg
+$app->post('/person', 'controllers\PersonController::route'); //create new person
+$app->get('/person/{uid}', 'controllers\PersonController::route'); //return person with basic info
+$app->get('/person/{uid}/all', 'controllers\PersonController::route'); //return person with alll info ex avg
+$app->get('/person/{uid}/photo/{width}/{height}', 'controllers\PersonController::route'); //return persons profile picture
+$app->patch('/person/{uid}/update', 'controllers\PersonController::route'); //update person information
+$app->get('/members', 'controllers\MemberController::route'); //return all members with basic info
+$app->get('/members/all', 'controllers\MemberController::route'); //return all members with all info ex avg
+$app->get('/members/current', 'controllers\MemberController::route'); //return all current members with basic info
+$app->get('/members/former', 'controllers\MemberController::route'); //return all former members with basic info
+$app->get('/members/candidate', 'controllers\MemberController::route'); //return all candidate members with basic info
 
 $app->run();
